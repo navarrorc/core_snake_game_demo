b7fd3637ff43bde7047059a74e4fbb054ec98112
Roberto C Navarro <rnavarro70@gmail.com>
2017-12-28 16:50:53 -0600

    update

--- core_snake_game_demo/Program.cs
+++ core_snake_game_demo/Program.cs
@@ -14,12 +14,26 @@ namespace core_snake_game_demo
         static char[][] grid = new char[20][];
         static int width = 50;
         static int height = 20;
+        static bool gameover = false;
+        static int worm_x = 25;
+        static int worm_y = 9;
+        enum Direction { UP = 1, DOWN, LEFT, RIGHT }
+        static Direction current = Direction.RIGHT;
+
+        static int lengthtimer = 0;
+        static int lengthtime = 8;
+        static int wormlength = 5;
+        static int target_x;
+        static int target_y;
+        static int score = 0;
 
         static List<Pos> worm = new List<Pos>();
 
         static void Main(string[] args)
         {
-            //System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
             var currentForegroundColor = Console.ForegroundColor;
 
             //Console.WriteLine("Hello World!");
@@ -27,6 +41,34 @@ namespace core_snake_game_demo
             DrawFrame();
             InitWorm();
             DrawWorm();
+            SetTarget();
+            InitScore();
+
+            while (!gameover)
+            {
+                DrawWormHead();
+                if (TargetTaken())
+                {
+                    SetTarget();
+                    UpdateScore();
+                }
+                Pause();
+                ReadKeys();
+                DrawWormBodyOnHeadPosition();
+                MoveWormHead();
+
+                if (isGameOver())
+                    gameover = true;
+
+                IncreaseWormLength();
+                DeleteWormTail();
+            }
+
+            DrawWormHead();
+            Console.SetCursorPosition(0, 20);
+            Console.WriteLine("Game Over");
+
+
 
             // reset foreground color
             Console.ForegroundColor = currentForegroundColor;
@@ -112,6 +154,171 @@ namespace core_snake_game_demo
                     Console.Write('o');
                 else
                     Console.Write('ö');
+            }
+        }
+
+        static void DrawWormHead()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.SetCursorPosition(worm_x, worm_y);
+            Console.Write('ö');
+        }
+
+        static void DrawWormBodyOnHeadPosition()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.SetCursorPosition(worm_x, worm_y);
+            Console.Write('o');
+        }
+
+        static void MoveWormHead()
+        {
+            grid[worm_y][worm_x] = 'o';
+
+            switch (current)
+            {
+                case Direction.UP:
+                    worm_y--;
+                    break;
+                case Direction.DOWN:
+                    worm_y++;
+                    break;
+                case Direction.LEFT:
+                    worm_x--;
+                    break;
+                case Direction.RIGHT:
+                    worm_x++;
+                    break;
+                default:
+                    break;
+            }
+
+            worm.Add(new Pos() { X = worm_x, Y = worm_y });
+        }
+
+        static bool isGameOver()
+        {
+            bool value = false;
+
+            if (grid[worm_y][worm_x] != ' ')
+                value = true;
+
+            return value;
+        }
+
+        static bool TargetTaken()
+        {
+            return worm_x == target_x && worm_y == target_y;
+        }
+
+        static void SetTarget()
+        {
+            Random rnd = new Random();
+            int x = 0;
+            int y = 0;
+
+            while (grid[y][x] != ' ')
+            {
+                x = rnd.Next(1, width - 1);
+                y = rnd.Next(1, height - 1);
+            }
+
+            target_x = x;
+            target_y = y;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(x, y);
+            Console.Write('☼');
+        }
+
+        static void InitScore()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.SetCursorPosition(51, 0);
+            Console.Write("Score: 0");
+        }
+
+        static void UpdateScore()
+        {
+            score++;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.SetCursorPosition(59, 0);
+            Console.Write(score);
+            //PlayScoreSound();
+        }
+
+        static void DeleteWormTail()
+        {
+            Console.SetCursorPosition(worm[0].X, worm[0].Y);
+            Console.Write(' ');
+            if (worm.Count != wormlength)
+            {
+                grid[worm[0].Y][worm[0].X] = ' ';
+                worm.RemoveAt(0);
+            }
+        }
+
+        static void IncreaseWormLength()
+        {
+            lengthtimer++;
+
+            if (lengthtimer == lengthtime)
+            {
+                lengthtimer = 0;
+                wormlength++;
+            }
+        }
+
+        static void Pause()
+        {
+            System.Threading.Thread.Sleep(100);
+        }
+
+        static void ReadKeys()
+        {
+            ConsoleKeyInfo s;
+
+            if (Console.KeyAvailable)
+            {
+                s = Console.ReadKey(true);
+
+                switch (s.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (current != Direction.DOWN)
+                        {
+                            current = Direction.UP;
+                            //PlayMoveSound();
+                        }
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        if (current != Direction.UP)
+                        {
+                            current = Direction.DOWN;
+                            //PlayMoveSound();
+                        }
+                        break;
+
+                    case ConsoleKey.LeftArrow:
+                        if (current != Direction.RIGHT)
+                        {
+                            current = Direction.LEFT;
+                            //PlayMoveSound();
+                        }
+                        break;
+
+                    case ConsoleKey.RightArrow:
+                        if (current != Direction.LEFT)
+                        {
+                            current = Direction.RIGHT;
+                            //PlayMoveSound();
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
             }
         }
     }
